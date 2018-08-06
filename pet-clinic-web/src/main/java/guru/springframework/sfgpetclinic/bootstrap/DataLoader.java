6b344b1a2b0fba96a633a44e63a08445a22da063
jiveace <straightedgescotsman@gmail.com>
2018-08-06 14:48:41 +0100

    Added Pets and Contact info to owners on startup. Closes #33.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -38,12 +41,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Ken");
         owner1.setLastName("Patera");
+        owner1.setAddress("123 Bricker Lane");
+        owner1.setCity("Edinburgh");
+        owner1.setTelephone("01314478469");
+
+        Pet rover = new Pet();
+        rover.setPetType(savedDogPetType);
+        rover.setOwner(owner1);
+        rover.setName("Rover");
+        rover.setBirthDate(LocalDate.now());
+        owner1.getPets().add(rover);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Tetsuya");
         owner2.setLastName("Naito");
+        owner1.setAddress("123 Knife Crime Lane");
+        owner1.setCity("Glasgow");
+        owner1.setTelephone("01415568947");
+
+        Pet teasel = new Pet();
+        teasel.setPetType(savedCatPetType);
+        teasel.setOwner(owner2);
+        teasel.setName("Teasel");
+        teasel.setBirthDate(LocalDate.now());
+        owner2.getPets().add(teasel);
 
         ownerService.save(owner2);
 
